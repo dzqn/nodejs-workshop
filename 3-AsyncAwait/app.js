77c59f9a9b1f61b9907be2e493175fbbfc7afba7
Düzgün <duzguntutar@gmail.com>
2020-03-19 21:56:41 +0300

    async awat

---
+++ 3-AsyncAwait/app.js
@@ -0,0 +1,35 @@
+
+const user = { name: "Ali", Surname: "Kaya" };
+const userFriends = [
+    { name: "Ayşe", Surname: "Akar" },
+    { name: "Veli", Surname: "Kaya" },
+    { name: "Kaya", Surname: "Kaya" }
+];
+
+let GetUser = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(user);
+        }, 2000);
+    });
+}
+
+let GetUserFriend = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(userFriends);
+        }, 500);
+    });
+}
+
+async function main() {
+    let a = await GetUser();
+    let b = await GetUserFriend();
+    console.log(a);
+    console.log(b);
+}
+
+main();
+
+
+
