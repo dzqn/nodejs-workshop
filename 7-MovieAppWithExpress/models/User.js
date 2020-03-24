c6969e992bc1f4065e357d5410857eb10d696ca0
Düzgün <duzguntutar@gmail.com>
2020-03-24 19:57:56 +0300

    user

---
+++ 7-MovieAppWithExpress/models/User.js
@@ -0,0 +1,9 @@
+mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+   username:String,
+   password:String
+});
+
+module.exports = mongoose.model('user',UserSchema);
--- 7-MovieAppWithExpress/routes/index.js
+++ 7-MovieAppWithExpress/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const User = require('../models/User');
+
+router.post('/register',(req,res,nex)=> {
+  const {username,password} = req.body;
+  const user = new User({
+    username,
+    password
+  })
+
+  const promise = user.save();
+
+  promise.then(data => {
+    res.json(data);
+  })
+
+})
+
 module.exports = router;
