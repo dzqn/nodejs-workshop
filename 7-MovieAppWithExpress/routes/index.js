5e91ceccd4d47b0e0cbc9374ec3d8811a8cb4c41
Düzgün <duzguntutar@gmail.com>
2020-03-24 21:13:03 +0300

    bcrypt

--- 7-MovieAppWithExpress/routes/index.js
+++ 7-MovieAppWithExpress/routes/index.js
@@ -2,25 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
-router.post('/register',(req,res,nex)=> {
-  const {username,password} = req.body;
-  const user = new User({
-    username,
-    password
-  })
+router.post('/register', (req, res, nex) => {
+  const { username, password } = req.body;
 
-  const promise = user.save();
+  bcrypt.hash(password, 10).then(hash => {
+    const user = new User({
+      username,
+      password: hash
+    })
 
-  promise.then(data => {
-    res.json(data);
-  })
+    const promise = user.save();
 
+    promise.then(data => {
+      res.json(data);
+    })
+
+  });
 })
 
 module.exports = router;
